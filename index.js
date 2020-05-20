6506744d16482fbb3427c3084df8e2741c422bf2
Sylvain <sylvain@sbx.pm>
2020-05-20 11:52:17 +0200

    updated plugin to match last specs

--- index.js
+++ index.js
@@ -7,20 +7,22 @@ const erbString = new RegExp('\'<%(.*?)%>\'', 'g');
 const erbExpression = new RegExp('<%(.*?)%>', 'g');
 const erbBlock = new RegExp('<%([sS])*?%>([sS])*?<%([sS])*?(end|END)([sS])*?%>', 'g');
 
-// import processors
-module.exports.processors = {
-  '.erb': {
-    preprocess: function (text, filename) {
-      const lintableText = text
-        .replace(erbString, '\'Ignored Ruby String.\'')
-        .replace(erbBlock, '/* \'Ignored Ruby Block.\' */')
-        .replace(erbExpression, '\'Ignored Ruby Expression.\'');
+// export processor
+module.exports = {
+  processors = {
+    'js-erb': {
+      preprocess: function (text, filename) {
+        const lintableText = text
+          .replace(erbString, '\'Ignored Ruby String.\'')
+          .replace(erbBlock, '/* \'Ignored Ruby Block.\' */')
+          .replace(erbExpression, '\'Ignored Ruby Expression.\'');
 
-      return [lintableText];
-    },
-    postprocess: function (messages, filename) {
-      return messages[0];
+        return [lintableText];
+      },
+      postprocess: function (messages, filename) {
+        return messages[0];
+      },
+      supportsAutofix: true
     }
   }
 };
-
